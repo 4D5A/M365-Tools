40a5103dbcbb4a91b931dbac3c6e8557d658b6a3
4D5A <4D5A@users.noreply.github.com>
2022-09-12 20:26:12 -0400

    update

--- Azure Active Directory/Disable-AzureADUser.ps1
+++ Azure Active Directory/Disable-AzureADUser.ps1
@@ -1,8 +1,9 @@
-If (-Not (Get-Module -Name AzureAD)) {
+Import-Module -Name AzureAD -ErrorAction:SilentlyContinue
+If(-Not (Get-Module -Name AzureAD)) {
   [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
   Install-Module -Name AzureAD -Force
+  Import-Module -Name AzureAD
 }
-Import-Module -Name AzureAD -ErrorAction:SilentlyContinue -UseWindowsPowerShell
 
 $File = "Disable-ADAADUser_log_$(Get-Date -Format ddMMyyyy_HHMMss).txt"
 $Logfilepath = "$env:USERPROFILE\Desktop\"
@@ -12,15 +13,22 @@ Clear-Host
 Get-AzureADUser | Select-Object DisplayName, UserPrincipalName, AccountEnabled
 $UPN = Read-Host "Enter the User Principal Name of the account which is suspected of being compromised"
     $DisableAzureADUserDisableConfirm = Read-host "If you continue, the AzureAD user $UPN will be disabled and active AzureAD tokens used for authentication by $UPN will be revoked. Do you want to proceed? (Yes/No)"
-    If ($DisableAzureADUserDisableConfirm -like "Yes") {
+    If($DisableAzureADUserDisableConfirm -like "Yes") {
         "Disabling $UPN" | Tee-Object -FilePath "$Logfilepath\$File" -Append | Write-Host -ForegroundColor Green
-        Get-AzureADUser -ObjectID $UPN | Set-AzureADUser -AccountEnabled $false
+        Set-AzureADUser -ObjectId $UPN -AccountEnabled $false
         "Revoking all Azure Active Directory tokens issued for $UPN" | Tee-Object -FilePath "$Logfilepath\$File" -Append | Write-Host -ForegroundColor Green
-        Get-AzureADUser -ObjectID $UPN | Revoke-AzureADUserAllRefreshToken
+        Revoke-AzureADUserAllRefreshToken -ObjectId $UPN
+        $DisableAzureADUsersDevices = Read-host "Do you want to disable $UPN's Azure Active Directory devices from connecting to Microsoft 365? (Yes/No)"
+        If($DisableAzureADUsersDevices -like "Yes") {
+            "Disabling access from $UPN's Azure Active Directory Devices" | Tee-Object -FilePath "$Logfilepath\$File" -Append | Write-Host -ForegroundColor Green
+            Get-AzureADUserRegisteredDevice -ObjectId $UPN | Set-AzureADDevice -AccountEnabled $false
+        }
         "Getting the Azure Active Directory account status of $UPN" | Tee-Object -FilePath "$Logfilepath\$File" -Append | Write-Host -ForegroundColor Green
         Get-AzureADUser -ObjectID $UPN | Select-Object UserPrincipalName, AccountEnabled
+        "Getting the Azure Active Directory status of devices registered to $UPN" | Tee-Object -FilePath "$Logfilepath\$File" -Append | Write-Host -ForegroundColor Green
+        Get-AzureADUserRegisteredDevice -All:$True -ObjectId $UPN | Select-Object ObjectId, DisplayName, DeviceOSType, DeviceOSVersion, DeviceTrustType, ProfileType, AccountEnabled
     }
-    If ($DisableAzureADUserDisableConfirm -notlike "Yes") {
+    If($DisableAzureADUserDisableConfirm -notlike "Yes") {
         Write-Host "You decided not to disable the AzureAD user $UPN and revoke all AzureAD tokens for AzureAD user $UPN. You also indicated you believe the AzureAD account $UPN is compromised. If you meant to disable the AzureAD account $UPN and revoke all AzureAD tokens for the AzureAD account $UPN, please run the script again and answer the questions correctly."
     }
 Disconnect-AzureAD -Confirm:$false
